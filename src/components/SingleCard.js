17baa248a63b6ea04afde0693c9b0d5d1228076f
Aaron Bruce <aaronbruce555@gmail.com>
2020-07-06 12:11:20 -0400

    Add images of the cards and organize the background images into their own folder

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -4,8 +4,13 @@ import Tarot from '../../data/datasets_tarot-images.json';
 const SingleCard = () => {
     const [tarotCard, setTarotCard] = useState(null);
     const [errors, setErrors] = useState([]);
-    
+
     // pick a random card
+    const getRandomCard = () => {
+        // randomly select a suit, randomly select 1 card from that suit
+        // Trump has 21 cards, each lesser has 14
+        const suits = ['Trump', 'Cups', 'Swords', 'Wands', 'Pentacles']
+    }
     // pick a random major arcana card (bonus)
 
 
