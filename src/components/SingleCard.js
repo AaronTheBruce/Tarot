91c028136d594c354875229ee2c25f7e78a4ce8e
Aaron Bruce <aaronbruce555@gmail.com>
2020-07-06 11:09:12 -0400

    Add tarot json dataset and mock up of SingleCard component

---
+++ src/components/SingleCard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Tarot from '../../data/datasets_tarot-images.json';
+
+const SingleCard = () => {
+    const [tarotState, setTarotState] = useState(null);
+
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
