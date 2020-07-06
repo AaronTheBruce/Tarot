ea9c1d63f4cbd38f035a895de9628726a8d729a3
Aaron Bruce <aaronbruce555@gmail.com>
2020-07-06 12:37:53 -0400

    Move images into src folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import SingleCard from './components/SingleCard';
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <>
+      <h1>Hello world!</h1>
+      <SingleCard />
+    </>
   );
 }
 
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Tarot from '../../data/datasets_tarot-images.json';
 
 const SingleCard = () => {
@@ -9,14 +9,23 @@ const SingleCard = () => {
     const getRandomCard = () => {
         // randomly select a suit, randomly select 1 card from that suit
         // Trump has 21 cards, each lesser has 14
-        const suits = ['Trump', 'Cups', 'Swords', 'Wands', 'Pentacles']
+        const suits = ['Trump', 'Cups', 'Swords', 'Wands', 'Pentacles'];
+        /**
+         * Idea: I could randomly grab 1 suit, and randomly grap 1 of its members..
+         * However, to maintain the essence of tarot, I would shuffle the deck and directly
+         * draw any cards needed for the spread. Assuming I'm not choosing from deck cuts.
+         *
+         * Question: What are the pros and cons of each approach?
+         */
     }
     // pick a random major arcana card (bonus)
 
 
     return (
         <div>
-
+            <h1>Single Card Component</h1>
         </div>
     )
 }
+
+export default SingleCard;
