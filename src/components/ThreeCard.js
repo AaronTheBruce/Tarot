bc2d877d3217d78b37667ce701f5df809f6bc35b
Aaron Bruce <aaronbruce555@gmail.com>
2020-07-07 18:40:00 -0400

    Add ThreeCard spread with Position labels

--- src/components/ThreeCard.js
+++ src/components/ThreeCard.js
@@ -33,25 +33,17 @@ const ThreeCard = () => {
             {spread ?
                 <div className="three_spread">
                     {spread.map((card, i) =>
-                        <div className="card">
-                            <div><img className="three_card_image" src={`images/cards/${card.img}`} alt={card.name} /></div>
-                            <div className="card-details">
-                                <div className="card-name">{card.number}: {card.name}</div>
-                                <div className="keywords">Keywords: {renderKeywords(card)}</div>
-                                <div className="meaning-light">
-                                    <div className="label">
-                                        Light:
-                                </div>
+                        <div className="card" key={card.name}>
+                            {i === 0 ? <div>Action to Avoid</div> :
+                                i === 1 ? <div>The Situation</div> :
+                                    <div>Action to Take</div>}
+                            <img className="three_card_image" src={`images/cards/${card.img}`} alt={card.name} />
+                            <div className="three_card_details">
+                                <div className="three_card_name">{card.number}: {card.name}</div>
+                                <div className="three_card_keywords">Keywords: <div>{renderKeywords(card)}</div></div>
+                                <div className="three_card_fortune_telling">
                                     <ul>
-                                        {card.meanings.light.map(item => <li key={item}>{item}</li>)}
-                                    </ul>
-                                </div>
-                                <div className="meaning-dark">
-                                    <div className="label">
-                                        Shadow:
-                                </div>
-                                    <ul>
-                                        {card.meanings.shadow.map(item => <li key={item}>{item}</li>)}
+                                        {card.fortune_telling.map(item => <li key={item}>{item}</li>)}
                                     </ul>
                                 </div>
                             </div>
@@ -61,7 +53,6 @@ const ThreeCard = () => {
                 :
                 <div className="prompt">Shuffle and Draw a Spread</div>}
             <button type="submit" onClick={() => getThisSpread(3)} >Get Spread</button>
-            <button type="submit" onClick={() => console.log(spread)} >Spread?</button>
         </div>
     )
 }
