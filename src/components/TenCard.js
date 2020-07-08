a073acbec3d6fb523f959a560d323df5e0c4f3e0
Aaron Bruce <aaronbruce555@gmail.com>
2020-07-08 19:25:40 -0400

    Add Details for card on TenCard spread. Click to view a card's details

--- src/components/TenCard.js
+++ src/components/TenCard.js
@@ -3,7 +3,8 @@ import { getDeck, getSpread, spliceShuffle, stackSuffle, riffleShuffle, renderKe
 
 const TenCard = () => {
     const [tarotDeck, setTarotDeck] = useState(null);
-    const [spread, setSpread] = useState(null)
+    const [spread, setSpread] = useState(null);
+    const [card, setCard] = useState(null);
 
     const getThisDeck = () => {
         setTarotDeck(getDeck());
@@ -19,6 +20,7 @@ const TenCard = () => {
     }
 
     const getThisSpread = (num) => {
+        shuffle();
         setSpread(getSpread(tarotDeck, num));
     }
 
@@ -29,6 +31,31 @@ const TenCard = () => {
             return;
         }
     });
+
+    // const tenCardImage = document.querySelector('.ten_card_image');
+
+    // tenCardImage.addEventListener('click', (event) => {
+    //     event.target.classList.add('focus');
+    // });
+
+    const cardHandler = (index) => {
+        if (card === spread[index]) {
+            setCard(null);
+        }
+        else if (card !== spread[index]) {
+            setCard(null);
+            setCard(spread[index]);
+        }
+        else {
+            console.log("Card is", card);
+            console.log("spread[index]", spread[index]);
+            alert("You've hit an exceptional circumstance, what'd you just do? Check log for details");
+        }
+    }
+
+    const toggleDisplay = (e) => {
+        e.target.classList.toggle('problem')
+    }
 
     return (
         <div className="options">
@@ -43,6 +70,7 @@ const TenCard = () => {
                     <li>Shuffle will shuffle the deck 3 times</li>
                     <li>Reset will set the deck in order</li>
                     <li>Get Spread shuffles once and lays out a 3 card spread</li>
+                    <li>Click the horizontal card in the middle to move it for a better view of 'Position By Problem'</li>
                 </ul>
             </div>
             {spread ?
@@ -53,7 +81,8 @@ const TenCard = () => {
                             <div className="position_title">Conscious Thoughts</div>
                             <img className="ten_card_image"
                                 alt={spread[2].name}
-                                src={`images/cards/${spread[2].img}`}>
+                                src={`images/cards/${spread[2].img}`}
+                                onClick={() => { cardHandler(2) }}>
                             </img>
                             {/* <div>{spread[3].number} {spread[3].name}</div> */}
                         </div>
@@ -62,32 +91,36 @@ const TenCard = () => {
                             <div className="position_title">Recent Past</div>
                             <img className="ten_card_image"
                                 alt={spread[4].name}
-                                src={`images/cards/${spread[4].img}`}>
+                                src={`images/cards/${spread[4].img}`}
+                                onClick={() => { cardHandler(4) }}>
                             </img>
                             {/* <div>{spread[4].number} {spread[4].name}</div> */}
-                            </div>
+                        </div>
                         <div className="situation">
                             <div className="position_title">Present By Problem</div>
-                            <div className="present">
-                                <img className="ten_card_image"
-                                    alt={spread[0].name}
-                                    src={`images/cards/${spread[0].img}`}>
-                                </img>
-                                {/* <div>{spread[0].number} {spread[0].name}</div> */}
-                            </div>
-                            <div className="problem">
-                                <img className="ten_card_image"
-                                    alt={spread[1].name}
-                                    src={`images/cards/${spread[1].img}`}>
-                                </img>
-                                {/* <div>{spread[1].number} {spread[1].name}</div> */}
-                            </div>
+                            {/* <div className="present"> */}
+                            <img className="present ten_card_image"
+                                alt={spread[0].name}
+                                src={`images/cards/${spread[0].img}`}
+                                onClick={(e) => { cardHandler(0); }}>
+                            </img>
+                            {/* <div>{spread[0].number} {spread[0].name}</div> */}
+                            {/* </div> */}
+                            {/* <div className="problem"> */}
+                            <img className="problem ten_card_image"
+                                alt={spread[1].name}
+                                src={`images/cards/${spread[1].img}`}
+                                onClick={(e) => { cardHandler(1); toggleDisplay(e); }}>
+                            </img>
+                            {/* <div>{spread[1].number} {spread[1].name}</div> */}
+                            {/* </div> */}
                         </div>
                         <div className="future">
                             <div className="position_title">Near Future</div>
                             <img className="ten_card_image"
                                 alt={spread[5].name}
-                                src={`images/cards/${spread[5].img}`}>
+                                src={`images/cards/${spread[5].img}`}
+                                onClick={(e) => { cardHandler(5) }}>
                             </img>
                             {/* <div>{spread[5].number} {spread[5].name}</div> */}
                         </div>
@@ -96,7 +129,8 @@ const TenCard = () => {
                             <div className="position_title">Unconscious Thoughts</div>
                             <img className="ten_card_image"
                                 alt={spread[3].name}
-                                src={`images/cards/${spread[3].img}`}>
+                                src={`images/cards/${spread[3].img}`}
+                                onClick={() => { cardHandler(3) }}>
                             </img>
                             {/* <div>{spread[3].number} {spread[3].name}</div> */}
                         </div>
@@ -107,7 +141,8 @@ const TenCard = () => {
                             <div className="position_title">Final Outcome</div>
                             <img className="ten_card_image"
                                 alt={spread[9].name}
-                                src={`images/cards/${spread[9].img}`}>
+                                src={`images/cards/${spread[9].img}`}
+                                onClick={() => { cardHandler(9) }}>
                             </img>
                             {/* <div>{spread[9].number} {spread[9].name}</div> */}
                         </div>
@@ -115,7 +150,8 @@ const TenCard = () => {
                             <div className="position_title">Hopes and Fears</div>
                             <img className="ten_card_image"
                                 alt={spread[8].name}
-                                src={`images/cards/${spread[8].img}`}>
+                                src={`images/cards/${spread[8].img}`}
+                                onClick={() => { cardHandler(8) }}>
                             </img>
                             {/* <div>{spread[8].number} {spread[8].name}</div> */}
                         </div>
@@ -123,7 +159,8 @@ const TenCard = () => {
                             <div className="position_title">Your Environment</div>
                             <img className="ten_card_image"
                                 alt={spread[7].name}
-                                src={`images/cards/${spread[7].img}`}>
+                                src={`images/cards/${spread[7].img}`}
+                                onClick={() => { cardHandler(7) }}>
                             </img>
                             {/* <div>{spread[7].number} {spread[7].name}</div> */}
                         </div>
@@ -131,12 +168,53 @@ const TenCard = () => {
                             <div className="position_title">Your Self</div>
                             <img className="ten_card_image"
                                 alt={spread[6].name}
-                                src={`images/cards/${spread[6].img}`}>
+                                src={`images/cards/${spread[6].img}`}
+                                onClick={() => { cardHandler(6) }}>
                             </img>
                             {/* <div>{spread[6].number} {spread[6].name}</div> */}
                         </div>
                     </div>
+
+                    {card ?
+                        <div className="card">
+                            
+                            <img className="focused-card-image"
+                                src={`images/cards/${card.img}`}
+                                alt={card.name} />
+
+                            <div className="card-details">
+                                <div className="card-name">{card.number}: {card.name}</div>
+                                <div className="keywords">Keywords: {renderKeywords(card)}</div>
+                                <div className="meaning">
+                                    <div className="label">
+                                        Light:
+                                    </div>
+                                    <ul>
+                                        {card.meanings.light.map(item => <li key={item}>{item}</li>)}
+                                    </ul>
+                                </div>
+                                <div className="meaning">
+                                    <div className="label">
+                                        Shadow:
+                                    </div>
+                                    <ul>
+                                        {card.meanings.shadow.map(item => <li key={item}>{item}</li>)}
+                                    </ul>
+                                </div>
+                                <div className="meaning">
+                                    <div className="label">
+                                        Questions to Ask Yourself
+                                </div>
+                                    <ul>
+                                        {card.QuestionsToAsk.map(item => <li key={item}>{item}</li>)}
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        :
+                        <div></div>}
                 </div>
+
                 :
                 <div>Shuffle and Draw</div>}
         </div>
