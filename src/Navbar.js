d3bf2b582f8106dc7059e55d298e7b57275a9709
Aaron Bruce <aaronbruce555@gmail.com>
2020-09-23 15:49:50 -0400

    Add a footer, needs better styling, maybe just put into the index.html?

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,7 @@ import SingleCard from './components/SingleCard';
 import ThreeCard from './components/ThreeCard';
 import TenCard from './components/TenCard';
 import Tips from './components/Tips';
+import About from './components/About';
 
 export const Nav = () => {
     return (
@@ -13,11 +14,13 @@ export const Nav = () => {
                 <NavLink to="/ThreeCard">Three Card Spread</NavLink>
                 <NavLink to="/TenCard">Ten Card Spread</NavLink>
                 <NavLink to="/">Tips</NavLink>
+                <NavLink to="/about">About</NavLink>
             </nav>
             <Switch>
                 <Route path="/SingleCard" render={() => <SingleCard />} />
                 <Route path="/ThreeCard" render={() => <ThreeCard />} />
                 <Route path="/TenCard" render={() => <TenCard />} />
+                <Route path="/About" render={() => <About />} />
                 <Route path="/" render={() => <Tips />} />
             </Switch>
         </BrowserRouter>
